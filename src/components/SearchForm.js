4f05f589b49e9e073bcbe782123848b050e6874c
Christopher Lien <cnlien@gmail.com>
2019-10-14 14:45:02 -0400

    search form code

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -5,13 +5,23 @@ import './components.css';
 
 export default function SearchForm({search}) {
   
+  const [searchChar, setSearchChar] = useState({
+    name: ''
+  });
+
+  const handleChange = e => {
+    setSearchChar({...searchChar, name: e.target.value});
+  };
+
   return (
     <section className="search-form">
-      <Form>
+      <Form onSubmit={() => search(searchChar)}>
         <Input
+          onChange={handleChange}
           className='searchbox'
           name='name'
           placeholder='Search Characters'
+          value = {searchChar.name}
         />
         <Button type="submit">Search</Button>
      </Form>
